3776678e1e1d73de18a7b7e302045999829ecc70
changzhenxing <changzx@cnnho.com>
2020-04-16 15:24:47 +0800

    集成zuul路由与zuul filter验证

---
+++ service-zuul/src/main/java/com/demo/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,21 @@
+package com.demo.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+//开启zuul功能
+@EnableZuulProxy
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
